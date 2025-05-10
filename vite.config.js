4c10a92a66b361548b1952e5f61ca5c1eb178da3
Yaman Verma <vermayaman244@gmail.com>
2025-05-10 20:09:54 +0530

    fix vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.cjs'
+  build: {
+    outDir: 'dist' // Explicitly set output directory
   }
 })
